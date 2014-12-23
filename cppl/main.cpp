00d4fd75968a693d0faa3c4a2db6c0f96f386b90
Michael Layzell <michael@thelayzells.com>
2014-12-22 19:54:11 -0500

    Initial Commit

---
+++ cppl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cppl
+//
+//  Created by Michael Layzell on 2014-12-22.
+//  Copyright (c) 2014 Michael Layzell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
